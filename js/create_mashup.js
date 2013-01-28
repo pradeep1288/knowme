95b0e4a9dbf02fbebd795d72323fd6a4a3f60615
Pradeep Nayak <pradeep1288@gmail.com>
2013-01-28 02:05:13 -0800

    Right now only dealing with coffee

--- js/create_mashup.js
+++ js/create_mashup.js
@@ -14,52 +14,14 @@ HowMany.prototype.run = function() {
  * @private
  */
 HowMany.prototype.onHistory = function(history) {
-  var restaurants_html = [];
-  var airports_html = [];
   var coffee_html = [];
-  var resident_html = [];
-  var multiplex_html = [];
-  var mall_html =[];
-  var restaurants_counts = 0;
-  var airports_counts = 0;
   var coffee_counts = 0;
-  var resident_counts = 0;
-  var multiplex_counts = 0;
-  var mall_counts = 0;
   for (var i = 0; i < history.length; i++) {
     var entry = history[i]['venue'];
     if (entry.categories.length != 0){
     if (entry.menu == undefined )
     {
       //deal with non food places here
-      if (entry.categories[0].name == 'Airport')
-      {
-        airports_counts++;
-        airports_html.push(entry.name + '<br/>');
-      }
-      else if (entry.categories[0].name == 'Mall')
-      {
-        mall_counts++;
-        mall_html.push(entry.name, ', ', entry.location.address, ', ', entry.location.city, '<br/>');
-      }
-      else if (entry.categories[0].name == 'Multiplex')
-      {
-        multiplex_counts++;
-        multiplex_html.push(entry.name, ', ', entry.location.address, ', ', entry.location.city, '<br/>');
-      }
-      else if (entry.categories[0].name.indexOf('Residential') > -1)
-      {
-        resident_counts++;
-        resident_html.push(entry.name, ', ', entry.location.address, ', ', entry.location.city, '<br/>');
-      }
-      else if (entry.categories[0].name.indexOf('Coffee') > -1) {
-        coffee_counts++;
-        coffee_html.push(entry.location.address, ', ', entry.location.city, '<br/>');
-      }  
-      else 
-      {
-        //Ignore others for now
-      }
     }
     else {
       //deal with food places here  
